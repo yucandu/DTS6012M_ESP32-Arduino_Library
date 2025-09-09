16eaa667c3f31b3c9116dc1acc99fafc49f1eef3
yucandu <moeburn@gmail.com>
2025-09-09 15:56:38 -0400

    more info

--- examples/UART_Stream.ino
+++ examples/UART_Stream.ino
@@ -12,10 +12,17 @@ void setup() {
 }
 
 void loop() {
-  uint16_t dist;
-  if (lidar.readFrame(dist)) {
-    Serial.print("Distance: ");
-    Serial.print(dist);
-    Serial.println(" mm");
+  DTS6012M_Frame f;
+  if (lidar.readFrame(f)) {
+    Serial.print("Primary: ");
+    Serial.print(f.primaryDistance);
+    Serial.print(" mm (Int ");
+    Serial.print(f.primaryIntensity);
+    Serial.print(")  Secondary: ");
+    Serial.print(f.secondaryDistance);
+    Serial.print(" mm (Int ");
+    Serial.print(f.secondaryIntensity);
+    Serial.print(")  Sunlight Base: ");
+    Serial.println(f.sunlightBase);
   }
 }
